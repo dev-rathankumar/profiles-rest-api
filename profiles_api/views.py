6c5423085578b120d92511725f8bd5b0511dea34
Rathan <rathan.kumar492@gmail.com>
2020-05-15 15:34:12 +0530

    Finished Profile Feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated    # IsAuthenticatedOrReadOnly
 
 from profiles_api import serializers
 from profiles_api import models
@@ -134,6 +134,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     permission_classes = (
         permissions.UpdateOwnStatus,
         IsAuthenticated
+        # IsAuthenticatedOrReadOnly
     )
 
     def perform_create(self, serializer):
